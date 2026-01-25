98828c8b8ab8b2462dd5e50faa74c7e2a4cde5b8
Ravin Chavez <ravinpaul.chavez@gmail.com>
2026-01-25 12:46:51 +0800

    (chore) added FoodClass in models (which also temporary comtains the menu, mealorder, and mealpackage)

---
+++ IMS.Domain/Models/FoodClass.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+enum FoodType { 
+    Viand,
+    Rice, 
+    Desert,
+}
+
+namespace IMS.Domain.Models
+{
+    internal class FoodClass
+    {
+        [Key]
+        Guid FoodID { get; set; }
+
+        String FoodName { get; set; }
+
+        decimal FoodPrice { get; set; }
+
+        // Blob or PNG of the food or something
+        // CRITICAL: Make sure that this can be NULLABLE.
+        // Don't fetch it if it's already CACHED or not NEEDED
+        // To reduce memory issues.
+
+        FoodType TypeOfFood { get; set; }
+
+        bool isAlaCarte { get; set; }
+        bool isAvailable { get; set; }
+    }
+
+    internal class Menu
+    {
+        [Key]
+        Guid MenuID { get; set; }
+        String MenuName { get; set; }
+        List<FoodClass> FoodsInMenu { get; set; }
+    }
+
+    internal class MealOrder
+    {
+        [Key]
+        Guid MealOrderID { get; set; }
+
+        // CRITICAL: Make sure that this order is 
+
+        DateTime OrderDate { get; set; }
+        List<FoodClass> FoodsOrdered { get; set; }
+        
+        decimal? TotalPrice { get; set; }      
+
+        // CHORE: evaluate this ONLY WHEN NEEDED, NOT NOW
+        //{ 
+        //    get 
+        //    {
+        //        decimal total = 0;
+        //        foreach (var food in FoodsOrdered)
+        //        {
+        //            total += food.FoodPrice;
+        //        }
+        //        return total;
+        //    }
+        //}
+    }
+
+    internal class MealPackage
+    {
+        [Key]
+        Guid MealPackageID { get; set; }
+        String PackageName { get; set; }
+        List<FoodClass> FoodsInPackage { get; set; }
+        decimal PackagePrice { get; set; }
+    }
+
+}
---
+++ IMS.Service/Program.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.OpenApi;
+
+
+// ABSOLUTELY DO NOT TOUCH THESE!
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "IMS API v1");
+        options.RoutePrefix = "swagger"; // Makes it available at /swagger
+    });
+}
+
+app.UseHttpsRedirection();
+
+
+
+
+
+
+
+
+
+
+
+
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.MapGet("/", () =>
+{
+    return "<h1>Hi</h1>";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
